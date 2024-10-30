453258e8a96b01b6188f3e202023ac0c88c96770
Yuto Nishida <yuto@berkeley.edu>
2024-10-29 18:41:43 -0700

    Init rust src

--- devenv.nix
+++ devenv.nix
@@ -26,6 +26,9 @@
 
   # https://devenv.sh/languages/
   # languages.nix.enable = true;
+  languages.rust = {
+    enable = true;
+  };
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
