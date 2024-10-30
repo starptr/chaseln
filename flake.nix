2c6ffab8bccfabb37a8a835b5fb633f84f2f1bba
Yuto Nishida <yuto@berkeley.edu>
2024-10-29 23:22:43 -0700

    Add nix formatter

--- flake.nix
+++ flake.nix
@@ -11,41 +11,59 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      devenv,
+      systems,
+      ...
+    }@inputs:
     let
       forEachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
-      packages = forEachSystem (system: let
+      packages = forEachSystem (
+        system:
+        let
           pkgs = import nixpkgs {
             inherit system;
-            config = { allowBroken = true; };
+            config = {
+              allowBroken = true;
+            };
           };
         in
         {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-        chaseln = pkgs.rustPackages.rustPlatform.buildRustPackage {
-          pname = "chaseln";
-          version = "0.1.0";
-          src = ./app;
-          cargoLock = {
-            lockFile = ./app/Cargo.lock;
-          };
-        };
-      });
-
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                (import ./devenv.nix)
-              ];
+          devenv-up = self.devShells.${system}.default.config.procfileScript;
+          chaseln = pkgs.rustPackages.rustPlatform.buildRustPackage {
+            pname = "chaseln";
+            version = "0.1.0";
+            src = ./app;
+            cargoLock = {
+              lockFile = ./app/Cargo.lock;
             };
-          });
+          };
+        }
+      );
+
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [ (import ./devenv.nix) ];
+          };
+        }
+      );
+      formatter = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.nixfmt-rfc-style
+      );
     };
 }
