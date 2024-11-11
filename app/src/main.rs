b3aab729059d2c29ba234cced15228635ced2e2a
Yuto Nishida <yuto@berkeley.edu>
2024-11-11 14:23:42 -0800

    Add a new state for checking a directory without a trailing slash

--- app/src/main.rs
+++ app/src/main.rs
@@ -57,31 +57,57 @@ impl std::fmt::Display for EntryKind {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+enum EntryPrefix {
+    /// No prefix for the first entry
+    First,
+    /// Most entries are a dereferenced symlink of the last entry
+    Dereferenced,
+    /// This entry is the same as the previous entry, without the trailing slash
+    TrimmedTrailingSlash,
+}
+
+impl std::fmt::Display for EntryPrefix {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            EntryPrefix::First => write!(f, ""),
+            EntryPrefix::Dereferenced => write!(f, "→ "),
+            EntryPrefix::TrimmedTrailingSlash => write!(f, "← "),
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Entry {
+    /// The canonical way to access this file in the filesystem
     abs_location: PathBuf,
+    /// How this file was referred to in the parent symlink
+    /// i.e. the data in the symlink that pointed to this file
     display: String,
     kind: EntryKind,
+    prefix: EntryPrefix,
 }
 impl Entry {
-    fn new(abs_location: &Path) -> Self {
+    fn new(abs_location: &Path, prefix: EntryPrefix) -> Self {
         Self {
             abs_location: abs_location.to_owned(),
             display: abs_location.to_string_lossy().into_owned(),
             kind: fs::symlink_metadata(&abs_location).into(),
+            prefix,
         }
     }
-    fn new_with_display(abs_location: &Path, display: &str) -> Self {
+    fn new_with_display(abs_location: &Path, prefix: EntryPrefix, display: &str) -> Self {
         Self {
             abs_location: abs_location.to_owned(),
             display: display.to_owned(),
             kind: fs::symlink_metadata(&abs_location).into(),
+            prefix,
         }
     }
 }
 impl std::fmt::Display for Entry {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} {}", self.kind, self.display)
+        write!(f, "{}{} {}", self.prefix, self.kind, self.display)
     }
 }
 
@@ -103,14 +129,26 @@ impl Iterator for EntryIteratorContext {
             return None;
         }
         let entry = self.current_entry.as_ref().unwrap();
-        if entry.kind != EntryKind::Symlink {
-            return self.current_entry.take();
+        match entry.kind {
+            EntryKind::Symlink => {
+                let symlink_content = fs::read_link(&entry.abs_location).ok()?;
+                let parent = entry.abs_location.parent()?;
+                let next_abs_location = any_path_to_abs(parent, &symlink_content);
+                let new_entry = Entry::new_with_display(&next_abs_location, EntryPrefix::Dereferenced, &symlink_content.to_string_lossy());
+                self.current_entry.replace(new_entry)
+            }
+            EntryKind::Directory => {
+                let loc_str = entry.abs_location.to_string_lossy();
+                if !loc_str.ends_with("/") {
+                    return self.current_entry.take();
+                }
+                // Slightly normalize the directory path
+                let next_abs_location = entry.abs_location.components().as_path();
+                let new_entry = Entry::new_with_display(next_abs_location, EntryPrefix::TrimmedTrailingSlash, &next_abs_location.to_string_lossy());
+                self.current_entry.replace(new_entry)
+            }
+            _ => self.current_entry.take(),
         }
-        let symlink_content = fs::read_link(&entry.abs_location).ok()?;
-        let parent = entry.abs_location.parent()?;
-        let next_abs_location = any_path_to_abs(parent, &symlink_content);
-        let new_entry = Entry::new_with_display(&next_abs_location, &symlink_content.to_string_lossy());
-        self.current_entry.replace(new_entry)
     }
 }
 
@@ -118,16 +156,12 @@ fn main() -> Result<()> {
     let args = cli::get_args();
     let filename = args.filename;
     // Slightly normalize the input path
-    let filename = filename.components().as_path();
+    // TODO: after every step, slight normalization should occur
+    //let filename = filename.components().as_path();
     let abspath = any_path_to_abs(&get_cwd()?, &filename);
     let it = EntryIteratorContext::new(&abspath);
-    let mut is_first = true;
     for entry in it {
-        if !is_first {
-            print!("→ ");
-        }
         println!("{}", entry);
-        is_first = false;
     }
     Ok(())
 }
